d5068da90d99d068ca92e4db677eeb88ab2e1020
IlhamWahed <ilham.wahid06@gmail.com>
2023-04-04 21:57:05 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(3, 5));
 const subtrack = (a, b) => a - b;
 console.log(subtrack(10, 3));
 
-const multiplay = (a, b) => a * b;
-console.log(multiplay(10, 3));
+const multiplayNumber = (a, b) => a * b;
+console.log(multiplayNumber(10, 3));
 
 const divide = (a, b) => a / b;
 console.log(divide(10, 2));
