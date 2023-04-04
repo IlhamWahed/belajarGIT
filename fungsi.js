c7f66868ac970d536adaa560d1556acabee56d47
IlhamWahed <ilham.wahid06@gmail.com>
2023-04-04 21:21:22 +0700

    menghapus file yang tidak perlu

--- fungsi.js
+++ fungsi.js
@@ -1,7 +1,5 @@
-console.log("fungsi satu");
-
 const add = (a, b) => a + b;
 console.log(add(3, 5));
 
-const deduct = (a, b) => a - b;
-console.log(deduct(10, 5));
+const subtrack = (a, b) => a - b;
+console.log(subtrack(10, 3));
